2c3df5c7b8052e21813a183ca6d20c6ff448c528
Rainbow Forge <rainbowforge@proton.me>
2026-08-19 16:59:59 -0400

    Update instances.ts
    Modified to redirect localhost:5173 API calls to 8090

--- src/lib/pocketbase/instances.ts
+++ src/lib/pocketbase/instances.ts
@@ -1,6 +1,12 @@
 import PocketBase from 'pocketbase'
-
 const marketplace_url = import.meta.env.VITE_MARKETPLACE_URL || 'https://main.marketplace.primo.page'
-
-export const self = new PocketBase(typeof location !== 'undefined' ? location.origin : 'http://127.0.0.1:3000')
+const getPBUrl = () => {
+  if (typeof location === 'undefined') return 'http://127.0.0.1:3000'                                   
+  if (location.port === '5173') {                                             
+    return 'http://localhost:8090' //                                                                   
+  }                                                                  
+  return location.origin                                                                           
+}                                                                             
+export const self = new PocketBase(getPBUrl())                                                       
 export const marketplace = new PocketBase(marketplace_url)
+                                            
